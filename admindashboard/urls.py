2242fe853c7cd5045cf65dd12ac87ff794ac2605
Carlos Panganiban <cgpanganiban@up.edu.ph>
2020-05-05 01:18:15 +0800

    Add skeletal templates for admin dashboard

--- admindashboard/urls.py
+++ admindashboard/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="admin-dashboard.index"),
-    path('users', views.users, name="admin-dashboard.index"),
-    path('logs', views.logs, name="admin-dashboard.index"),
+    path('users', views.users, name="admin-dashboard.users"),
+    path('logs', views.logs, name="admin-dashboard.logs"),
 ]
--- admindashboard/views.py
+++ admindashboard/views.py
@@ -1,13 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+debug_users = [
+    {
+        'email': 'test@debug.com',
+        'password': 'test',
+    },
+    {
+        'email': 'test2@debug.com',
+        'password': 'test2',
+    },
+]
+
 
 def index(request):
     return render(request, 'admindashboard/index.html')
 
 
 def users(request):
-    return HttpResponse("<h2>Admin dashboard users<h2>")
+    context = {
+        'users': debug_users,
+    }
+    return render(request, 'admindashboard/users.html', context)
 
 
 def logs(request):
